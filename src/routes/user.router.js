ca06b580adb4cc60dbcb61ec4165d7c33ea0dd19
KevinPaez <kevin.agustin-2000@outlook.es>
2025-12-13 11:46:45 -0300

    agrego ultimos cambios para la primera pre-entrega BACKEND II

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -21,8 +21,42 @@ router.post("/", async (req, res) => {
 
 // READ
 router.get("/", async (req, res) => {
-  const users = await UserModel.find();
-  res.json(users);
+  const users = await UserModel.find().select("-password");
+  res.json({ status: "success", users });
+});
+
+// READ POR ID
+router.get("/:uid", async (req, res) => {
+  const user = await UserModel.findById(req.params.uid).select("-password");
+
+  if (!user)
+    return res.status(404).json({ error: "Usuario no encontrado" });
+
+  res.json({ status: "success", user });
+});
+
+// UPDATE
+router.put("/:uid", async (req, res) => {
+  const updatedUser = await UserModel.findByIdAndUpdate(
+    req.params.uid,
+    req.body,
+    { new: true }
+  ).select("-password");
+
+  if (!updatedUser)
+    return res.status(404).json({ error: "Usuario no encontrado" });
+
+  res.json({ status: "success", user: updatedUser });
+});
+
+// DELETE
+router.delete("/:uid", async (req, res) => {
+  const deletedUser = await UserModel.findByIdAndDelete(req.params.uid);
+
+  if (!deletedUser)
+    return res.status(404).json({ error: "Usuario no encontrado" });
+
+  res.json({ status: "success", message: "Usuario eliminado" });
 });
 
 export default router;
